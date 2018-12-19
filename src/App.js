2a69239cbc2251e5fa9a256e1600e0a1c0c46809
a-vetrov <a.vetrov@gmail.com>
2018-12-19 21:52:15 +0300

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        React app
-          <div className="App-nav">
-                <a href="#home" className="App-nav-link">Home</a>
-                <a href="#portfolio" className="App-nav-link active">Portfolio</a>
-                <a href="#contacts" className="App-nav-link">Contacts</a>
-          </div>
-      </div>
-    );
-  }
+    render() {
+
+        return (
+            <div className="App">
+                <div className="App-nav">
+                    <NavLink activeClassName="active" to='/' className="App-nav-link" exact >Home</NavLink>
+                    <NavLink activeClassName="active" to='/portfolio' className="App-nav-link">Portfolio</NavLink>
+                    <NavLink activeClassName="active" to='/contacts' className="App-nav-link">Contacts</NavLink>
+                </div>
+                <div className="App-tab-content">
+                    <p>Some text</p>
+                    <p>Some text</p>
+                    <p>Some text</p>
+                    <p>Some text</p>
+                    <p>Some text</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
