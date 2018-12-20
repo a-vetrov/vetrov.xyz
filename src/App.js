131719aa21d6e4d6850bc92c0d36d94a38025440
a-vetrov <a.vetrov@gmail.com>
2018-12-20 19:03:00 +0300

    About updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom' ;
 import { Switch, Route } from 'react-router-dom' ;
 import './App.css';
-import About from './About' ;
+import About from './about/About' ;
 import Portfolio from './Portfolio' ;
 import Contacts from './contacts/Contacts' ;
 
@@ -13,7 +13,7 @@ class App extends Component {
         return (
             <div className="App">
                 <nav className="App-nav">
-                    <NavLink activeClassName="active" to='/' className="App-nav-link" exact >Home</NavLink>
+                    <NavLink activeClassName="active" to='/' className="App-nav-link" exact >О себе</NavLink>
                     <NavLink activeClassName="active" to='/portfolio' className="App-nav-link">Примеры работ</NavLink>
                     <NavLink activeClassName="active" to='/contacts' className="App-nav-link">Контакты</NavLink>
                 </nav>
@@ -24,6 +24,7 @@ class App extends Component {
                         <Route path='/contacts' component={Contacts}/>
                     </Switch>
                 </div>
+                <div className="App-copyright">© Андрей Ветров 2018</div>
             </div>
         );
     }
---
+++ src/about/About.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './About.css';
+
+class About extends Component {
+
+    constructor(props) {
+        super(props) ;
+        this.frotendNames = ['JavaScript', 'ES6', 'ES5', 'HTML5', 'CSS3', 'XML', 'JSON', 'AS3'] ;
+        this.frameworksNames = ['React', 'jQuery', 'Bootstrap'] ;
+        this.toolsNames = ['Webpack', 'Grunt', 'Jira', 'svn', 'git', 'Bitbucket', 'GitHub', 'npm'] ;
+        this.softwareNames = ['WebStorm', 'IntelliJ IDEA', 'Adobe Flash', 'Adobe Illustrator', 'Adobe Photoshop',
+            'MS Project', 'MS Word', 'MS Excel', 'MS PowerPoint', 'MS Outlook'] ;
+        this.languagesNames = ['Python', 'PHP', 'C++', 'Node.js'] ;
+
+        this.iconStyles = {
+            "AS3" : "as3", "Bitbucket" : "bitbucket", "Bootstrap" : "bootstrap", "C++" : "cpp",
+            "CSS3" : "css3", "ES5" : "es5", "ES6" : "es6", "MS Excel" : "excel", "Adobe Flash" : "flash",
+            "git" : "git", "GitHub" : "github", "Grunt" : "grunt", "HTML5" : "html5", "IntelliJ IDEA" : "idea",
+            "Adobe Illustrator" : "illustrator", "Jira" : "jira", "jQuery" : "jquery", "JavaScript" : "js",
+            "JSON" : "json", "Node.js" : "nodejs", "npm" : "npm", "MS Outlook" : "outlook",
+            "Adobe Photoshop" : "photoshop", "PHP" : "php", "MS PowerPoint" : "powerpoint", "MS Project" : "project",
+            "Python" : "python", "React" : "react", "svn" : "svn", "Webpack" : "webpack", "WebStorm" : "webstorm",
+            "MS Word" : "word", "XML" : "xml"
+
+        } ;
+    }
+
+    generateItems(names) {
+        return names.map( (s)  => {
+            return (<div className="About-item">
+                    <span className={'About-icon About-icon-' + this.iconStyles[s]} />
+                    {s}
+                </div> ) ;
+        } )
+    }
+
+    render() {
+
+        return (
+            <div>
+                <p>Меня зовут Андрей Ветров. Я занимаюсь frontend-разработкой с 2003 года. Долгое время занимался программированием на ActionScript.
+                После падения популярности Adobe Flash, перешел на JavaScript разработку.
+                </p>
+                <p>Имею опыт работы со следующими технологиями:</p>
+                <h2 className="About-h2 ">Frontend</h2>
+                <p className="About-row">{this.generateItems(this.frotendNames)}</p>
+                <h2 className="About-h2 ">Frameworks</h2>
+                <p className="About-row">{this.generateItems(this.frameworksNames)}</p>
+                <h2 className="About-h2 ">Инструменты</h2>
+                <p className="About-row">{this.generateItems(this.toolsNames)}</p>
+                <h2 className="About-h2 ">ПО</h2>
+                <p className="About-row">{this.generateItems(this.softwareNames)}</p>
+                <p>Немного знаком с языками программирования:</p>
+                <p className="About-row">{this.generateItems(this.languagesNames)}</p>
+            </div>
+        );
+    }
+}
+
+export default About;
