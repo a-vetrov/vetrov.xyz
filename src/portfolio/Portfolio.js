0b6abe1e089c37963d1ab92c13ad8bf73fca4f9c
a-vetrov <a.vetrov@gmail.com>
2018-12-25 23:22:29 +0300

    Geotool added

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -24,20 +24,37 @@ class Portfolio extends Component {
                     <a href="http://saal.ai/" target="_blank"  rel="noopener noreferrer"> Saal</a>.</body>
                     <details>
                         <p>
-                            Для верстки уроков мной был разработан универсальный XML-формат описания сценария, позволяющий задать последовательность действий, анимаций, вопросов и реакции на различные ответы ученика. Для проигрывания сценариев мной был создан плеер на JavaScript.
+                            Для верстки уроков мной был разработан универсальный XML-формат описания сценария,
+                            позволяющий задать последовательность действий, анимаций, вопросов и реакции на различные
+                            ответы ученика. Для проигрывания сценариев мной был создан плеер на JavaScript.
                         </p>
                         <p>
-                            На данной платформе было создано 20 пилотных уроков, которые проигрываются в нативном приложении с помощью WebView компоненты.
+                            На данной платформе было создано 20 пилотных уроков, которые проигрываются в нативном Android/iOS
+                            приложении с помощью WebView компоненты.
+                        </p>
+                        <p>
+                            При разработке использованы технологии: HTML5, CSS3, jQuery, Pure JS.
                         </p>
                     </details>
                 </Card>
-                <Card key="Saal2" onDetailsClick={this.handleDetailsClick.bind(this, "Saal2")} isOpen={this.state.openCardId === "Saal2"}>
-                    <header>Saal content player</header>
-                    <body><p>Разработка контент плеера для проигрыания интерактивных уроков на</p></body>
+                <Card key="Geotool" onDetailsClick={this.handleDetailsClick.bind(this, "Geotool")}
+                      isOpen={this.state.openCardId === "Geotool"}  exampleHref="./rm/geotool.html">
+                    <header>GeoTool</header>
+                    <body><p>Разработка инструмента для геометрических построений.</p></body>
                     <details>
-                        <p>Разработка контент плеера для проигрыания интерактивных уроков на </p>
-                        <p>Разработка контент плеера для проигрыания интерактивных уроков на </p>
-                        <p>Разработка контент плеера для проигрыания интерактивных уроков на </p>
+                        <p>
+                            Мной был разработан геометрический тул, позволяющий ученику строить  геометрические построения.
+                            Инструмент позволяет анализировать ответ ученика и проверять его правильность, исправлять
+                            неверные элементы при частично-правильном ответе, объяснять ответ в форме обучающей анимации.
+                        </p>
+                        <p>
+                            На основе данного тула было разработано множество интерактивных задач и анимаций теории,
+                            которые активно используются в продуктах
+                            <a href="https://www.reasoningmind.org/programs/foundations/" target="_blank" rel="noopener noreferrer"> Foundations </a>
+                            и <a href="https://www.reasoningmind.org/programs/staar-readiness/" target="_blank" rel="noopener noreferrer"> STAAR Readiness </a>
+                            компании ReasoningMind.
+                        </p>
+                        <p>При разработке использованы технологии: AS3, Adobe Flash.</p>
                     </details>
                 </Card>
                 <p>Portfolio some text</p>
